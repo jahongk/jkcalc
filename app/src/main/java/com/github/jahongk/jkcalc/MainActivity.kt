bdcbb7f2b5563ddd577f066a9cb3a11093b90239
jahong <jahongk@gmail.com>
2021-10-28 15:04:12 +0900

    add library module

--- app/src/main/java/com/github/jahongk/jkcalc/MainActivity.kt
+++ app/src/main/java/com/github/jahongk/jkcalc/MainActivity.kt
@@ -2,10 +2,20 @@ package com.github.jahongk.jkcalc
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.github.jahongk.calclib.Calc
+import com.github.jahongk.jkcalc.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        val calc = Calc()
+        val sum = calc.sum(1, 2)
+        binding.resultTextView.text = "$sum"
     }
 }
---
+++ calclib/src/main/java/com/github/jahongk/calclib/Calc.kt
@@ -0,0 +1,5 @@
+package com.github.jahongk.calclib
+
+class Calc {
+    fun sum(a: Int, b: Int) = a + b
+}
