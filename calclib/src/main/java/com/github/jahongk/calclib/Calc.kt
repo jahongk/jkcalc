57eca505488a947058c91247db5036bfa06c0688
jahong <jahongk@gmail.com>
2021-12-17 17:24:23 +0900

    calclib 에서 calclib-sub 의존성을 제거하고 테스트 >> 0.0.9

--- calclib/src/main/java/com/github/jahongk/calclib/Calc.kt
+++ calclib/src/main/java/com/github/jahongk/calclib/Calc.kt
@@ -1,10 +1,5 @@
 package com.github.jahongk.calclib
 
-import co.weverse.account.calclib.sub.CalcSub
-
 class Calc {
     fun sum(a: Int, b: Int) = a + b
-    fun printSub() = {
-        CalcSub().getSubString("calclib")
-    }
 }
